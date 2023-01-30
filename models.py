7c46e4d3edea3b55f0478c53a1be72b38dd166d6
LevytskyiS <levytskyi.sv@gmail.com>
2023-01-30 22:15:39 +0100

    Update models.py

--- models.py
+++ models.py
@@ -10,54 +10,50 @@ from sqlalchemy.sql.sqltypes import DateTime
 Base = declarative_base()
 
 
-class Groups(Base):
-    __tablename__ = "groups"
-    id = Column("id", Integer, primary_key=True)
-    group_name = Column(String(10), nullable=False, unique=True)
-    group_stud_relate = relationship(
-        "Students", cascade="all, delete", backref="groups"
-    )
-
-
-class Students(Base):
-    __tablename__ = "students"
-    id = Column("id", Integer, primary_key=True)
-    student = Column(String(255), nullable=False, unique=True)
-    group_id = Column(String(50), ForeignKey("groups.id", ondelete="CASCADE"))
-
-
-class Teachers(Base):
-    __tablename__ = "teachers"
-    id = Column("id", Integer, primary_key=True)
-    teacher = Column(String(255), nullable=False, unique=True)
-    teach_lesson_relate = relationship(
-        "Classes", cascade="all, delete", backref="classes"
-    )
-
-
-class Classes(Base):
-    __tablename__ = "classes"
-    id = Column("id", Integer, primary_key=True)
-    lesson = Column(String(255), nullable=False, unique=True)
-    teacher_id = Column(String(50), ForeignKey("teachers.id", ondelete="CASCADE"))
-
-
 marks = Table(
     "marks",
     Base.metadata,
     Column("id", Integer, primary_key=True),
     Column(
         "lesson_id",
-        ForeignKey("classes.id", ondelete="CASCADE"),
+        ForeignKey("classes.id"),
         Integer,
         nullable=False,
     ),
     Column(
         "student_id",
-        ForeignKey("students.id", ondelete="CASCADE"),
+        ForeignKey("students.id"),
         Integer,
         nullable=False,
     ),
     Column("mark", Integer, nullable=False),
     Column("created_at", DateTime, default=datetime.now()),
 )
+
+
+class Groups(Base):
+    __tablename__ = "groups"
+    id = Column("id", Integer, primary_key=True)
+    name = Column(String(10), nullable=False, unique=True)
+    student = relationship("Students", secondary=marks, backref="groups")
+
+
+class Students(Base):
+    __tablename__ = "students"
+    id = Column("id", Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    group_id = Column(String(50), ForeignKey("groups.id"))
+
+
+class Teachers(Base):
+    __tablename__ = "teachers"
+    id = Column("id", Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    classes = relationship("Classes", secondary=marks, backref="teachers")
+
+
+class Classes(Base):
+    __tablename__ = "classes"
+    id = Column("id", Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    teacher_id = Column(String(50), ForeignKey("teachers.id"))
