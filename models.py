1ac3cbe7e0c9a0f9300fbd28ab9ed0e09d8ea798
LevytskyiS <levytskyi.sv@gmail.com>
2023-01-31 18:49:05 +0100

    Update models.py

--- models.py
+++ models.py
@@ -10,50 +10,57 @@ from sqlalchemy.sql.sqltypes import DateTime
 Base = declarative_base()
 
 
-marks = Table(
-    "marks",
+stud_results = Table(
+    "stud_results",
     Base.metadata,
-    Column("id", Integer, primary_key=True),
+    Column("id", Integer, primary_key=True, autoincrement=True),
     Column(
-        "lesson_id",
-        ForeignKey("classes.id"),
+        "classes_id",
         Integer,
+        ForeignKey("classes.id"),
         nullable=False,
     ),
     Column(
         "student_id",
-        ForeignKey("students.id"),
         Integer,
+        ForeignKey("students.id"),
         nullable=False,
     ),
-    Column("mark", Integer, nullable=False),
+    Column("mark", Integer, ForeignKey("marks.id"), nullable=False),
     Column("created_at", DateTime, default=datetime.now()),
 )
 
 
+class Marks(Base):
+    __tablename__ = "marks"
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column(Integer, nullable=False, unique=True)
+    marks_relationship = relationship(stud_results)
+
+
 class Groups(Base):
     __tablename__ = "groups"
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column(String(10), nullable=False, unique=True)
-    student = relationship("Students", secondary=marks, backref="groups")
+    student = relationship("Students", secondary=stud_results, backref="groups")
 
 
 class Students(Base):
     __tablename__ = "students"
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False, unique=True)
-    group_id = Column(String(50), ForeignKey("groups.id"))
+    group_id = Column(Integer, ForeignKey("groups.id"))
 
 
 class Teachers(Base):
     __tablename__ = "teachers"
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False, unique=True)
-    classes = relationship("Classes", secondary=marks, backref="teachers")
+    classes = relationship("Classes", secondary=stud_results, backref="teachers")
 
 
 class Classes(Base):
     __tablename__ = "classes"
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False, unique=True)
-    teacher_id = Column(String(50), ForeignKey("teachers.id"))
+    teacher_id = Column(Integer, ForeignKey("teachers.id"))
